a8fc0dad9a9d1b85c42f785a8061351e192d569f
Alexandre GIRARD <alexandre.girard@maif.fr>
2026-08-20 09:53:24 +0000

    feat: ✨ add Vitest for testing and update package configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
@@ -8,5 +8,18 @@ export default defineConfig({
     proxy: {
       "/api": "http://localhost:3000",
     },
+  },
+  test: {
+    include: ["src/back-end/**/*.test.ts"],
+    exclude: ["node_modules/**"],
+    // environment: 'jsdom',
+    // coverage: {
+    //   provider: 'v8',
+    //   include: [
+    //     'src/back-end/**/*.ts',
+    //   ],
+    //   exclude: [],
+    //   reporter: ['text', 'html'],
+    // },
   },
 });
