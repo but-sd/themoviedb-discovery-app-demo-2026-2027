4aad24c12bc4dad152a59f59cb433ec0fc242f7f
Alexandre GIRARD <alexandre.girard@maif.fr>
2026-08-21 15:07:32 +0000

    feat: ✨ add AboutPage component and integrate it into App routing

--- src/front-end/App.tsx
+++ src/front-end/App.tsx
@@ -3,6 +3,7 @@ import "./app.css";
 import MovieDetailPage from "./pages/MovieDetailPage";
 import MoviesListPage from "./pages/MoviesListPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import AboutPage from "./pages/AboutPage";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Route path="/" element={<Navigate to="/movies" replace />} />
       <Route path="/movies" element={<MoviesListPage />} />
       <Route path="/movie/:id" element={<MovieDetailPage />} />
+      <Route path="/about" element={<AboutPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
---
+++ src/front-end/pages/AboutPage.tsx
@@ -0,0 +1,13 @@
+export default function AboutPage() {
+  return (
+    <main className="app-shell">
+      <header className="app-header">
+        <h1>À propos de l'application</h1>
+        <p>
+          Cette application est un projet de démonstration pour découvrir les films populaires et leurs détails. Elle
+          utilise une API pour récupérer les informations sur les films.
+        </p>
+      </header>
+    </main>
+  );
+}
