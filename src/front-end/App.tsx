bf0e323b552169ec8aa7efbc5d84a0bdc5e2afdf
Alexandre GIRARD <alexandre.girard@maif.fr>
2026-08-21 08:59:41 +0000

    feat: ✨ update routing to redirect from root to movies list page

--- src/front-end/App.tsx
+++ src/front-end/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import "./app.css";
 import MovieDetailPage from "./pages/MovieDetailPage";
 import MoviesListPage from "./pages/MoviesListPage";
@@ -7,7 +7,8 @@ import NotFoundPage from "./pages/NotFoundPage";
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<MoviesListPage />} />
+      <Route path="/" element={<Navigate to="/movies" replace />} />
+      <Route path="/movies" element={<MoviesListPage />} />
       <Route path="/movie/:id" element={<MovieDetailPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
