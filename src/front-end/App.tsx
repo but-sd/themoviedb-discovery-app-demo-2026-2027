8850fa66e0981de80d8de72d88e79d8dea3c4b50
Alexandre GIRARD <alexandre.girard@maif.fr>
2026-08-21 15:12:28 +0000

    feat: ✨ add NavBar component with navigation links and styles

--- src/front-end/App.tsx
+++ src/front-end/App.tsx
@@ -4,15 +4,19 @@ import MovieDetailPage from "./pages/MovieDetailPage";
 import MoviesListPage from "./pages/MoviesListPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import AboutPage from "./pages/AboutPage";
+import NavBar from "./components/NavBar/NavBar";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/movies" replace />} />
-      <Route path="/movies" element={<MoviesListPage />} />
-      <Route path="/movie/:id" element={<MovieDetailPage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Navigate to="/movies" replace />} />
+        <Route path="/movies" element={<MoviesListPage />} />
+        <Route path="/movie/:id" element={<MovieDetailPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
   );
 }
