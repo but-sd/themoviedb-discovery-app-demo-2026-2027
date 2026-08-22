e59db0865cc7d8745bd627a4589876c1cc631838
alexandre-girard <alexandre.girard@maif.fr>
2026-08-22 14:32:30 +0200

    feat: ✨ include front-end test files in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,6 @@ export default defineConfig({
     },
   },
   test: {
-    include: ["src/back-end/**/*.test.ts"],
+    include: ["src/back-end/**/*.test.ts", "src/front-end/**/*.test.tsx"],
   },
 });
